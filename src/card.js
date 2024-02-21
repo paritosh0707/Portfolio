79b0b5100f121afe50f999514903429e2dbd21f5
MK3103 <88797690+MK3103@users.noreply.github.com>
2024-02-22 01:56:24 +0530

    Routing added (#3)

--- src/card.js
+++ src/card.js
@@ -1,35 +1,16 @@
-// import React from 'react';
-// // import { useHistory } from 'react-router-dom'; // Assuming you're using React Router for navigation
+import React from "react";
+import "./Card.css"; // Import CSS file for styling (if needed)
+import { Link } from "react-router-dom";
 
-// function PortfolioCard({ imageUrl, title, description, link }) {
-// //   const history = useHistory();
-
-//   const handleClick = () => {
-//     // history.push(link); // Redirect to the specified link when clicked
-//   };
-
-//   return (
-//     <div className="portfolio-item" onClick={handleClick}>
-//       <img src={imageUrl} alt={title} />
-//       <h3>{title}</h3>
-//       <p>{description}</p>
-//     </div>
-//   );
-// }
-
-// export default PortfolioCard;
-
-
-import React from 'react';
-import './Card.css'; // Import CSS file for styling (if needed)
-
-const ProjectCard = ({ title }) => {
+const Card = () => {
   return (
     <div className="card">
-      <h2>{title}</h2>
-      {/* You can add more details or links here if needed */}
+      <button>
+        <Link to="/Project1">Go</Link>
+      </button>
+      <h2>Hello</h2>
     </div>
   );
-}
+};
 
-export default ProjectCard;
+export default Card;
